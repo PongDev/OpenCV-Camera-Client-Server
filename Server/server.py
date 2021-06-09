8bce6684e25672d136a8611adef308a41d2f6f34
PongDev <pongwiwat.dev@outlook.com>
2021-06-10 03:21:38 +0700

    Implement Server Video Stream Site

--- Server/server.py
+++ Server/server.py
@@ -2,7 +2,11 @@ import cv2
 import socket
 from dotenv import load_dotenv
 import os
+import numpy
 import pickle
+from flask import Flask, render_template, Response
+from flask.helpers import url_for
+import threading
 
 load_dotenv()
 
@@ -10,36 +14,88 @@ SERVER_PORT = int(os.getenv("SERVER_PORT"))
 SOCKET_HEADER_SIZE = int(os.getenv("SOCKET_HEADER_SIZE"))
 
 
-def recvall(sock, count):
-    buffer = b''
-    while count:
-        data = sock.recv(count)
-        if not data:
-            return None
-        buffer += data
-        count -= len(data)
-    return buffer
+class ReceiverServer (threading.Thread):
 
+    @staticmethod
+    def recvall(sock, count):
+        buffer = b''
+        while count:
+            data = sock.recv(count)
+            if not data:
+                return None
+            buffer += data
+            count -= len(data)
+        return buffer
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("", SERVER_PORT))
-sock.listen(True)
+    def __init__(self):
+        threading.Thread.__init__(self)
 
-print(f"Server Start On Port: {SERVER_PORT}")
+    def getFrame(self):
+        return self.frame
 
-while True:
-    conn, addr = sock.accept()
+    def run(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(("", SERVER_PORT))
+        self.sock.listen(True)
 
-    print(f"Receive Connection From: {addr[0]}:{addr[1]}")
+        print(f"Server Start On Port: {SERVER_PORT}")
 
-    try:
         while True:
-            length = int(recvall(conn, SOCKET_HEADER_SIZE).decode())
-            data = recvall(conn, length)
-            frame = pickle.loads(data)
-            cv2.imshow("Frame", frame)
-            cv2.waitKey(1)
-    except:
-        print(f"Connection Lost: {addr[0]}:{addr[1]}")
-    cv2.destroyAllWindows()
-sock.close()
+            self.frame = None
+
+            conn, addr = self.sock.accept()
+
+            print(f"Receive Connection From: {addr[0]}:{addr[1]}")
+
+            try:
+                while True:
+                    length = int(ReceiverServer.recvall(
+                        conn, SOCKET_HEADER_SIZE).decode())
+                    data = ReceiverServer.recvall(conn, length)
+                    self.frame = pickle.loads(data)
+            except:
+                print(f"Connection Lost: {addr[0]}:{addr[1]}")
+            cv2.destroyAllWindows()
+        self.sock.close()
+
+    def __del__(self):
+        cv2.destroyAllWindows()
+        self.sock.close()
+
+
+receiverServer = ReceiverServer()
+receiverServer.start()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return f'''
+    <body>
+        <div class="container">
+            <h1>Camera</h1>
+            <img src="{url_for('video_stream')}">
+        </div>
+    </body>
+    '''
+
+
+def gen_frames():
+    while True:
+        frame = receiverServer.getFrame()
+        if (type(frame) == type(None)):
+            frame = numpy.zeros([100, 100, 3], dtype=numpy.uint8)
+            frame.fill(0)
+        ret, frame = cv2.imencode('.jpg', frame)
+        frame = frame.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_stream')
+def video_stream():
+    return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+app.run()
