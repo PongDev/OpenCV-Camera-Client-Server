d2d6d2794cf233b1b5ca1016b6a887feab825ab0
PongDev <pongwiwat.dev@outlook.com>
2021-06-16 00:24:12 +0700

    Add Client Camera Rotate

--- Client/client.py
+++ Client/client.py
@@ -6,11 +6,13 @@ import numpy
 import pickle
 from datetime import datetime
 
-load_dotenv()
+load_dotenv(os.path.abspath(os.path.join(
+    os.path.realpath(__file__), "../.env")))
 
 SERVER_IP = os.getenv("SERVER_IP")
 SERVER_PORT = int(os.getenv("SERVER_PORT"))
 SOCKET_HEADER_SIZE = int(os.getenv("SOCKET_HEADER_SIZE"))
+CAMERA_ROTATE = int(os.getenv("CAMERA_ROTATE"))
 
 print("Start Client")
 print(f"Destination Server {SERVER_IP}:{SERVER_PORT}")
@@ -25,6 +27,8 @@ while True:
 
         while True:
             ret, frame = video.read()
+            if (CAMERA_ROTATE in [cv2.ROTATE_180, cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_90_COUNTERCLOCKWISE]):
+                frame = cv2.rotate(frame, CAMERA_ROTATE)
             frame = cv2.putText(frame, datetime.now().strftime(
                 "%Y-%m-%d %H:%M:%S"), (0, frame.shape[0]), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 255, 255), 2)
             data = pickle.dumps(numpy.array(frame))
